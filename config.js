18d1f3ac6c24ac55997744dae029a303826a6031
Lascience <sbaron@assessfirst.com>
2017-02-21 12:12:24 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const config = {
   recast: {
-    token: 'RECAST BOT TOKEN',
-    language: 'en',
+    token: '6a39bfabbff3f341b7baab47b15f4c9f',
+    language: 'fr',
   },
   slack: {
-    token: 'SLACK BOT TOKEN',
+    token: 'xoxb-143170063488-O3uA9aQM2n5PDkh3xrKWJ6Z2',
   },
   port: 8080,
 }
